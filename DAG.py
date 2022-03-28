74651dc006da408103c92a0f75dcc7b5e87b1d83
Ali Tamer Ünal <atu@icrontech.com>
2022-03-28 10:02:15 +0300

    First implementation of Bellman's shortest path formulation.

--- DAG.py
+++ DAG.py
@@ -1,29 +1,54 @@
 
 # not object oriented!!
 
-class DAG:
-    def __init__(self) -> None:
-         nodes = []
-         arcs = []
-
-         nodes.append(Node(1))
-         nodes.append(Node(2))         
-         nodes.append(Node(3))
-
-         arcs.append(nodes[0], nodes[1], 7)
-         arcs.append(nodes[0], nodes[2], 10)
-         arcs.append(nodes[1], nodes[2], 5)
-
 class Node:
     def __init__(self, code: str) -> None:
         self.code = code
-    
+        
+class DAG:
+    def __init__(self) -> None:
+         self.nodes = []
+         self.arcs = []
 
+         self.nodes.append(Node(1))
+         self.nodes.append(Node(2))         
+         self.nodes.append(Node(3))
+
+         self.arcs.append(Arc(self.nodes[0], self.nodes[1], 3))
+         self.arcs.append(Arc(self.nodes[0], self.nodes[2], 10))
+         self.arcs.append(Arc(self.nodes[1], self.nodes[2], 5))
+
+    def incomming_arcs(self, v: Node):
+        # Discuss performance
+
+        inarcs = []
+        for a in self.arcs:
+            if (a.to_node == v):
+                inarcs.append(a)
+        return inarcs
+
+    def SP(self, s : Node, v: Node) -> int:
+        # Discuss performance
+
+        min = 0
+        for a in self.incomming_arcs(v):
+            length = a.distance + self.SP(s, a.from_node)
+            if (min <= 0 or length < min):
+                min = length
+
+        return min
 class Arc:
     def __init__(self, from_node: Node, to_node: Node, distance: int) -> None:
         self.from_node = from_node
         self.to_node = to_node
         self.distance = distance
+
+d = DAG()
+
+# Really. Not object oriented!!
+print (d.SP(d.nodes[0], d.nodes[2]))
+
+
 
 
 
--- Shortest path dynamic programming formulation.md
+++ Shortest path dynamic programming formulation.md
@@ -9,15 +9,13 @@ Suppose s > --- > u > --- > v is a shortest path from s to v. This implies that 
 This can be proven by contradiction. If there is a shorter path between s and u, we can replace s > --- > u with the shorter path in s > --- > u > --- > v, and this would yield a better path between s and v. But we assumed that
 s > --- > u > --- > v is a shortest path between s and v, so have a contradiction.
 
-Based on this optimal sub-structure, we can write down the recursive formulation of the single source shortest path problem as the following:
-
 ## Formulation
 
-Let G = (N, A) be a graph with node set N and arc set A. Let d(u, v) be the length of the arc (u, v) in A.
+Let G = (N, A) be a graph with node set N and arc set A. Let d(u, v) > 0 be the length of the arc (u, v) in A.
 
 Define SP(s, v) as the length of the shorthest path between nodes s and v for and s and v in N:
 
-SP(s, v) = min {SP(s, u) + d(u, v) | (u, v) in A}
+SP(s, v) = min { SP(s, u) + d(u, v) | (u, v) in A }
 
 ## Discussion point
 
